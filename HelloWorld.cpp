96b9c2a0b301e63b63f3442f7583fb1b8778779e
Kaitlin Berryman <k.berrygirl.10@gmail.com>
2019-05-19 03:21:07 -0400

    Create HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>  // include libraries
+
+int main() 
+{
+  // std::cout is Character OUTput stream ... it prints ... pronounced "see-out"
+  std::cout << "Hello World!\n";
+
+  // terminal notes
+  // compile with gcc or g++
+  // execute with a.exe or ./a.out
+
+}
